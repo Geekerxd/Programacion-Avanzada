a06328ea52eb8ca1c2c7b2b5630e8338822fb0d7
Gonza Agui Gale <geeker00513@gmail.com>
2019-05-16 14:50:11 -0500

    TRABAJANDO EN LA VERCION FINAL DE LA AGENDA PARA UNA VETERINARIA

---
+++ Agenda para una Veterinaria v3/agenda nombre/resource.h
@@ -0,0 +1,62 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Veterinario.rc
+//
+#define IDD_DIALOG1                     101
+#define IDR_MENU1                       102
+#define IDD_Ven_Inf                     103
+#define IDD_Ven_Ace                     106
+#define IDD_Doc_Inf                     108
+#define IDD_alta                        110
+#define IDD_baja                        112
+#define IDD_ver_agenda                  114
+#define IDD_modi_cita                   116
+#define IDC_Edi_Inf                     1001
+#define IDC_BUTTON1                     1002
+#define IDC_BUTTON2                     1003
+#define IDC_BUTTON3                     1004
+#define IDC_SCROLLBAR1                  1004
+#define IDC_BUTTON4                     1005
+#define IDC_EDIT1                       1005
+#define IDC_EDIT2                       1006
+#define IDC_COMBO1                      1007
+#define IDC_EDIT3                       1009
+#define IDC_EDIT4                       1010
+#define IDC_EDIT5                       1011
+#define IDC_DATETIMEPICKER2             1018
+#define IDC_FECHA                       1018
+#define IDC_DATETIMEPICKER3             1019
+#define IDC_HORA                        1019
+#define IDC_AGREGAR                     1021
+#define IDC_LIST1                       1022
+#define IDC_hola                        1023
+#define IDC_hi                          1024
+#define IDC_h                           1025
+#define IDC_hi2                         1025
+#define IDC_bienve                      1025
+#define ID_VER_INFORMAION               40001
+#define ID_AYUDA_ACERCADELPROGRAMA      40002
+#define ID_ACERCA                       40003
+#define ID_MENU_INFORMACIONDELDOCTOR    40004
+#define ID_MENU_                        40005
+#define ID_MENU_40006                   40006
+#define ID_MENU_40007                   40007
+#define ID_MENU_S                       40008
+#define ID__DARDEALTA                   40009
+#define ID__DARDEBAJA                   40010
+#define ID__MODIFICARUNACITA            40011
+#define ID_EDITAR                       40012
+#define ID__VERAGENDA                   40013
+#define ID__MODIFICAR_CITA              40014
+#define ID_salir                        40015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        118
+#define _APS_NEXT_COMMAND_VALUE         40016
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
